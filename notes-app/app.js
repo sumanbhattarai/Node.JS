83f7c0f00bb744181afc8125b2050584c58373db
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-14 19:03:49 +0545

    Initial Commit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,15 @@
 
-// we must import fs module
+//The fs module provides an API for interacting with the file system.
+//To use this module:
+
 const fs = require('fs');
+
+// to create a new file
 fs.writeFileSync('file.txt' , 'Hello World ! ') ;
+
+// to append the existing file
+// fs.appendFileSync('file.txt' , 'Hello World ! ') ;
+
+
+
+
