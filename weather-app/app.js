f8d8d8b4c313bea686e57927961e09f911e98ae7
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-27 21:05:17 +0545

    Updated

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 
 const request = require('request')
+const chalk = require('chalk')
 
-const url = 'https://api.darksky.net/forecast/41f57b0025108c3d1b7d34edcaa7db60/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/41f57b0025108c3d1b7d34edcaa7db60/37.8267,-122.4233?units=si'
 request({url : url , json: true} , (error , response)=>{
     const currently = response.body.currently
-    console.log(`Today's temperature is ${currently.temperature} and chance to rain is ${currently.precipProbability}`)
+    const daily = response.body.daily
+    console.log(`Today's temperature is ${currently.temperature} degree Celcius and chance to rain is ${currently.precipProbability}% .`)
+    console.log(`${chalk.yellow.inverse('Conclusion')} : ${chalk.red(daily.data[0].summary)}`)
 })
