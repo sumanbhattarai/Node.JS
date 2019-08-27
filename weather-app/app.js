65d5666a072a860730ade11290dbdad3ef592692
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-08-27 20:47:49 +0545

    Currenlty Updated

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,7 @@
 const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/41f57b0025108c3d1b7d34edcaa7db60/37.8267,-122.4233'
-request({url : url} , (error , response)=>{
-    console.log(response)
+request({url : url , json: true} , (error , response)=>{
+    const currently = response.body.currently
+    console.log(`Today's temperature is ${currently.temperature} and chance to rain is ${currently.precipProbability}`)
 })
