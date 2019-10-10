c86e6370346c06997f9c1aebb11148a524620bd6
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-10-10 20:14:16 +0545

    initial Commit

---
+++ Challenge/objects.js
@@ -0,0 +1,33 @@
+
+// object shorthand
+
+const name = 'Suman'
+
+// If we want to use the property with value from a variable having property name and variable name same
+// then we can use shorthand.
+const person = {
+    name , // it is same as writing name : name . 
+    age : 20
+}
+
+console.log(person.name)
+console.log(person.age)
+
+
+// Object Destructing
+
+const info = {
+    infoName : 'X' ,
+    infoAge : 30 
+}
+
+// rating will have value 5 if it doent have any value defined in info object.
+const {infoName , infoAge , rating = 5} = info
+console.log(infoName) // X
+console.log(infoAge) // 30
+console.log(rating) //undefined
+
+// if we want a new name then
+
+const { infoName: newName } = info
+console.log(newName)
