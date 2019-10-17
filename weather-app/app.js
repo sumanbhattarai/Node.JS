6daf2262de5506569db5c84f29f830097b7d83f2
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-10-17 10:28:39 +0545

    Making it Simpler

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,80 +1,17 @@
 
-const request = require('request') // to handle http request
 const chalk = require('chalk') // to ouput with color in terminal
 const geoCode = require('./utils/geoCode.js')
+const getWeather = require('./utils/getWeather.js')
 
-//declaring a url variable to store the url of dark sky api
-//const url =  'https://api.darksky.net/forecast/41f57b0025108c3d1b7d34edcaa7db60/37.8267,-122.4233?units=si' 
 
-//request contains two argument . First is an options object which outlines what we want to do . 
-// It is where we specify our url.
-// Second one is function.
-
-// request({url : url , json : true} , (error , response)=>{
-//     if(error)
-//     {
-//         console.log(`${chalk.red('Unable to connect to dark sky API.')}`)
-//     }
-//     else if (response.body.error)
-//     {
-//         console.log(`${chalk.red('Unable to find location.')}`)
-//     }
-//     else
-//     {
-//         // console.log(`${chalk.green('Connected Successfully')}`)
-//         console.log(`Currently, it is ${chalk.yellow(response.body.currently.temperature)} degree celcius. There is ${chalk.yellow(response.body.currently.humidity)} % chances of raining.`)
-//     }
-// })
-
-const getWeather = ( lat , long , callback)=> {
-    const url = 'https://api.darksky.net/forecast/41f57b0025108c3d1b7d34edcaa7db60/'+lat+','+long+'?units=si' 
-    request({url : url , json:true } , (error , response)=>{
-        if(error)
-        {
-            callback('Unable to connect to dark sky API.' , undefined)
-        }
-        else if(response.body.error)
-        {
-            callback('Unable to find the location.' , undefined)
-
-        }
-        else
-        {
-            callback(undefined , {
-                temperature : response.body.currently.temperature ,
-                humidity : response.body.currently.humidity
-            })
-        }
-
-    })
-
-}
-
+// Fetching weather forecast
 getWeather (37.8267  , -122.4233 , (error , data)=>{
     console.log('Error : ' , error )
     console.log('Data : ' , data)
 })
 
-// geocoding.
-// const urlGeo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoic3VtYW5iaGF0dGFyYWkiLCJhIjoiY2sxc3plbHpqMDA5dTNjcXM0ZnNldmV6biJ9.h3Oq798TEzuOq1t06fXM9A&limit=1'
-// request({url : urlGeo , json: true} , (error , response)=>{
-//     if(error)
-//     {
-//         console.log(`${chalk.red('Unable to connect to Map box API')}`)
-//     }
-//     else if (response.body.features.length === 0)
-//     {
-//         console.log(`${chalk.red('Unable to find the location.')}`)
-//     }
-//     else
-//     {
-//         console.log(`Los angeles co-ordinate is ${chalk.yellow(response.body.features[0].center[0])}, ${chalk.yellow(response.body.features[0].center[1])}`)
-//     }
 
-// })
-
-
-
+// GeoCoding
 geoCode('Bharatpur Nepal' , (error , data)=>{
     console.log('Error : ' ,  error )
     console.log('Data : ' ,  data)
