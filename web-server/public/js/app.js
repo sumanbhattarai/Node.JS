b038096bd2ed70b1f45c43d8f668938d0d195174
sumanbhattarai <sumanbhattaraiofficial@gmail.com>
2019-10-18 20:52:51 +0545

    Finishing touch with loader animation

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,16 +6,25 @@ const formData = document.querySelector('form')
 formData.addEventListener('submit'  , (e)=>{
     e.preventDefault()
     let address = document.querySelector('#address').value
-
+    document.querySelector('.loader').style.display = 'block'
     fetch('http://localhost:3000/weather?address='+address).then((response)=>{
+
     response.json().then((data)=>{
+        document.querySelector('.loader').style.display = 'none'
         if(data.error)
         {
-            console.log(data.error)
+            document.querySelector('#output-0').innerHTML = data.error
+            document.querySelector('#output-1').innerHTML = ''
+            document.querySelector('#output-2').innerHTML = ''
+            document.querySelector('#output-3').innerHTML =  ''
         }
         else
         {
-            console.log(data)
+            document.querySelector('#output-0').innerHTML = ''
+            document.querySelector('#output-1').innerHTML = "Temperature : " + data.temperature
+            document.querySelector('#output-2').innerHTML = "Probability of Raining : " + data.humidity
+            document.querySelector('#output-3').innerHTML =  "Location : " + data.place
+
         }
     })
 })
--- web-server/src/utils/geoCode.js
+++ web-server/src/utils/geoCode.js
@@ -3,7 +3,7 @@ const request = require('request')
 
 const geoCode = ( address , callback )=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoic3VtYW5iaGF0dGFyYWkiLCJhIjoiY2sxc3plbHpqMDA5dTNjcXM0ZnNldmV6biJ9.h3Oq798TEzuOq1t06fXM9A&limit=1'
-    request( {url, json : true} , (error , {body})=>{
+    request( {url, json : true} , (error , {body}={})=>{
         if(error)
         {
             callback('Unable to connect to Map box API' , undefined)
